1dbaaf776bf4c996b300ceed10f89a06171d4ea3
surelynobody161 <surelynobody161@gmail.com>
2025-06-25 10:04:52 +0200

    user merge geifxt

--- models/User.js
+++ models/User.js
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
+    user_id: { type: Number, unique: true, required: true },
     name: { type: String, maxlength: 50, required: true },
     email: { type: String, maxlength: 50, unique: true, required: true },
-<<<<<<< HEAD
     password: { type: String, maxlength: 100, required: true },
     role: { type: String, maxlength: 20 },
-=======
-    password_hash: { type: String, maxlength: 60, required: true, select: false },
->>>>>>> 489eae762a5a11fdda5a645f572992aab7e75b5d
     created_at: { type: Date, default: Date.now }
 });
 
