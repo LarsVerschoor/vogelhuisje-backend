3925d28e42c80859f6393da614b5e306952e4683
surelynobody161 <surelynobody161@gmail.com>
2025-06-25 09:55:57 +0200

    index gefixt

--- index.js
+++ index.js
@@ -3,56 +3,10 @@ configEnv();
 
 import express from 'express';
 import http from 'http';
-<<<<<<< HEAD
-import mongoose from 'mongoose';
-<<<<<<< HEAD
-import cors from 'cors';
-
-import router from './routes/index.js';
-=======
-import cors from 'cors'
-
-await mongoose.connect(process.env.MONGODB_URI);
-
-const app = express();
-const server = http.createServer(app);
-app.use(express.json());
-app.use(cors());
-
-app.use(express.json());
-
-import router from './routes/index.js';
-app.use('/', router);
-
-import usersRouter from './routes/users.js';
-app.use('/users', usersRouter);
-
-import shopsRouter from './routes/shopitems.js';
-app.use('/shopitems', shopsRouter);
-
-import notesRouter from './routes/notes.js';
-app.use('/notes', notesRouter);
-
-import rentalsRouter from './routes/rentals.js';
-app.use('/rentals', rentalsRouter);
-
-import camerasRouter from './routes/cameras.js';
-app.use('/cameras', camerasRouter);
-
-import recordingsRouter from './routes/recordings.js';
-app.use('/recordings', recordingsRouter);
-
-import birdhouseRouter from './routes/birdhouses.js';
-app.use('/birdhouses', birdhouseRouter);
-
->>>>>>> 489eae762a5a11fdda5a645f572992aab7e75b5d
-import User from './models/User.js';
-=======
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
 import shopitems from './routes/shopitems.js';
->>>>>>> Koens-BE
 
 const app = express();
 app.use(cors({ origin: '*' }));
