80eebeb3ccbcfed4ef18eb418332e88a7ecdc47e
LarsVerschoor <larsverschoor2@gmail.com>
2025-06-26 21:36:40 +0200

    Converted mongodb decimal objects to numbers

--- routes/birdhouses.js
+++ routes/birdhouses.js
@@ -23,7 +23,11 @@ router.get('/', async (req, res) => {
             // Convert string to actual boolean
             query.is_available = req.query.is_available === 'true';
             let birdhouses = await Birdhouse.find({ is_available: true});
-            console.log(req.query);
+            birdhouses.map((birdhouse) => ({
+                ...birdhouse.toObject(),
+                price: parseFloat(birdhouse.price.toString()),
+                oldPrice: birdhouse.oldPrice ? parseFloat(birdhouse.oldPrice.toString()) : undefined
+            }));
             res.status(200).json(birdhouses);
         } else {
             const birdhouses = await Birdhouse.find();
